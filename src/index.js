eebe3b7d73a78c988e0d41806766addcb5eab3b5
Petar Petrov <ppetrov@vexelon.net>
2020-04-09 19:11:19 +0200

    Rename var

--- src/index.js
+++ src/index.js
@@ -4,28 +4,28 @@ import Bulstat from "./bulstat";
 import IBAN from "./iban";
 
 class BGCN {
-  static personalNumber = (number) => {
-    return new PersonalNumber(number);
+  static personalNumber = (value) => {
+    return new PersonalNumber(value);
   };
 
-  static pn = (number) => {
-    return this.personalNumber(number);
+  static pn = (value) => {
+    return this.personalNumber(value);
   };
 
-  static egn = (number) => {
-    return new EGN(number);
+  static egn = (value) => {
+    return new EGN(value);
   };
 
-  static bulstat = (number) => {
-    if (typeof number === "string" && number.length === 10) {
+  static bulstat = (value) => {
+    if (typeof value === "string" && value.length === 10) {
       // 10 знака е Булстат, който е еднакъв с ЕГН на търговеца
-      return new EGN(number);
+      return new EGN(value);
     }
-    return new Bulstat(number);
+    return new Bulstat(value);
   };
 
-  static iban = (number) => {
-    return new IBAN(number);
+  static iban = (value) => {
+    return new IBAN(value);
   };
 }
 
